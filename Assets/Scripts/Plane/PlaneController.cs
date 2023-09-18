3c24ce4e266ca6410d6cac791956a1047d722392
Javid Ahmed <javidahmed@icloud.com>
2023-09-18 23:23:57 +0100

    Refactor some methods

--- Assets/Scripts/Plane/PlaneController.cs
+++ Assets/Scripts/Plane/PlaneController.cs
@@ -6,7 +6,6 @@ public class PlaneController : MonoBehaviour
     Rigidbody rb;
     public Transform propeller;
 
-
     Vector3 planeStart;
     Vector3 planeOrientation
     {
@@ -19,11 +18,12 @@ public class PlaneController : MonoBehaviour
             ) / 180;
         }
     }
+    Quaternion propellerStartOrientation;
 
     // Config
-    public float maxThrust = 200f;
-    public float lift = 135f;
-    public float responsiveness = 20f;
+    [SerializeField] float maxThrust = 200f;
+    [SerializeField] float lift = 135f;
+    [SerializeField] float responsiveness = 20f;
 
     // Motion
     float throttle = 100f;
@@ -43,56 +43,59 @@ public class PlaneController : MonoBehaviour
     // Neuroevolution
     [HideInInspector] public NeuralNetwork nn;
     int inputNodes;
-    int outputNodes;
-    float[] output;
     [HideInInspector] public bool isAlive = true;
     [HideInInspector] public bool isStopped = false;
-    int aliveCount = 0;
     float cumulativeDistance = 0f;
-    float cumulativeThrottle = 0f;
 
     // Target
     Transform target;
-    Vector3 startDifFromTarget { get { return target.position - planeStart; } }
+    Vector3 startDifFromTarget;
     Vector3 currentDifFromTarget { get { return target.position - transform.position; } }
-    float distanceToTarget { get { return Vector3.Distance(transform.position, target.position); } }
+    float distanceToTarget { get { return Vector3.Magnitude(currentDifFromTarget); } }
 
     private void Awake()
     {
         target = GameObject.FindWithTag("Target").transform;
         rb = GetComponent<Rigidbody>();
+        planeStart = transform.position;
+        startDifFromTarget = currentDifFromTarget;
+        propellerStartOrientation = propeller.rotation;
 
         // Configuring neural network
         nn = GetComponent<NeuralNetwork>();
         inputNodes = nn.layerNodes[0];
-
-        outputNodes = nn.layerNodes[nn.layerNodes.Length - 1];
-        output = new float[outputNodes];
-
-        planeStart = transform.position;
-
     }
 
-    float[] input 
-    { get
+    float[] input
+    {
+        get
         {
             float[] _input = new float[inputNodes];
-            _input[0] = currentDifFromTarget.x / startDifFromTarget.x;
-            _input[1] = currentDifFromTarget.y / startDifFromTarget.y;
-            _input[2] = currentDifFromTarget.z / startDifFromTarget.z;
+            _input[0] = currentDifFromTarget.x / startDifFromTarget.x / 2;
+            _input[1] = currentDifFromTarget.y / startDifFromTarget.y / 2;
+            _input[2] = currentDifFromTarget.z / startDifFromTarget.z / 2;
             _input[3] = planeOrientation.x;
             _input[4] = planeOrientation.y;
             _input[5] = planeOrientation.z;
-            _input[6] = rb.velocity.magnitude / 50f;
+            _input[6] = rb.velocity.x / rb.velocity.magnitude;
+            _input[7] = rb.velocity.y / rb.velocity.magnitude;
+            _input[8] = rb.velocity.z / rb.velocity.magnitude;
             return _input;
+        }
+    }
+
+    float[] output
+    {
+        get
+        {
+            return nn.feedforward(input);
+
         }
     }
 
     private void Move()
     {
         if (isStopped) return;
-
-        output = nn.feedforward(input);
 
         throttle = Mathf.SmoothStep(throttle, output[0] * 100, Time.deltaTime / smoothStepScaling);
         throttle = Mathf.Clamp(throttle, 0f, 100f);
@@ -139,17 +142,14 @@ public class PlaneController : MonoBehaviour
     private void Update()
     {
         Move();
-
-        propeller.Rotate(Vector3.forward * throttle);
         cumulativeDistance += distanceToTarget;
-        cumulativeThrottle += throttle;
-        aliveCount++;
     }
 
     private void FixedUpdate()
     {
         // Move plane forward
         rb.AddForce(-transform.right * maxThrust * throttle);
+        propeller.Rotate(Vector3.forward * throttle);
 
         // Rotate plane
         rb.AddTorque(transform.right * roll * responseModifier);
@@ -165,9 +165,7 @@ public class PlaneController : MonoBehaviour
         // Set attributes
         isAlive = true;
         isStopped = false;
-        aliveCount = 0;
         cumulativeDistance = 0f;
-        cumulativeThrottle = 0f;
 
         // Reset motion
         roll = 0f;
@@ -179,12 +177,12 @@ public class PlaneController : MonoBehaviour
         // Move to spawn position
         transform.position = planeStart;
         transform.rotation = Quaternion.identity;
+        propeller.rotation = Quaternion.Euler(357.22699f, 270f, 0f);
     }
 
-    // Update fitness
     public float fitness()
     {
-        float _fitness = 1 / (Mathf.Pow(distanceToTarget, 3) + 0.01f);
+        float _fitness = 1 / (Mathf.Pow(cumulativeDistance, 3) + 0.01f);
         float scaling = isAlive ? 1f : 0.01f;
         return _fitness * scaling;
     }
